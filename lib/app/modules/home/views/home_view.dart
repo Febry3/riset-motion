a0808597c02eb2895d054b498d1ff35d656f1c3b
Fehehehe <febryt22@gmail.com>
2025-02-07 13:31:22 +0700

    feat: adding navigation bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:riset_motion/app/themes/font.dart';
+import 'package:riset_motion/app/themes/spaces.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,14 +12,57 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Selamat Datang,",
+                      style: Title2,
+                    ),
+                    Text(
+                      "Pak Tani Tano",
+                      style: Title1SemiBold,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(width: 2, color: Colors.grey),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Icon(Iconsax.notification),
+                      ),
+                    ),
+                    sbX12,
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(width: 2, color: Colors.grey),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Icon(Iconsax.user),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/app/modules/navigation/views/navigation_view.dart
+++ lib/app/modules/navigation/views/navigation_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:riset_motion/app/modules/home/views/home_view.dart';
 import 'package:riset_motion/app/themes/colors.dart';
 import 'package:riset_motion/app/themes/font.dart';
 
@@ -11,11 +12,14 @@ class NavigationView extends GetView<NavigationController> {
   const NavigationView({super.key});
   @override
   Widget build(BuildContext context) {
+    List<Widget> _pages = [
+      HomeView(),
+    ];
     return Scaffold(
       body: Obx(
         () => IndexedStack(
           index: controller.selectedIndex.value,
-          children: [],
+          children: [_pages[controller.selectedIndex.value]],
         ),
       ),
       bottomNavigationBar: SizedBox(
